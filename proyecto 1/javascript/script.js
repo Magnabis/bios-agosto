884f85cfc0dd033a687df53c840fdd7246826271
Magnabis <gilesagustin@gmail.com>
2024-10-25 01:08:56 -0300

    first commit

---
+++ proyecto 1/javascript/script.js
@@ -0,0 +1,20 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskList = document.getElementById("taskList");
+  const newTask = taskInput.value;
+
+  if (newTask !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = newTask;
+    taskList.appendChild(listItem);
+    taskInput.value = "";
+  }
+}
+
+
+
+taskInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
